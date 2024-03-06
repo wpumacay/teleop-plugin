329ef32d65b6b68af3ecde0d734df02d9ffe2c44
wpumacay <wpumacay@gmail.com>
2024-03-06 15:22:48 -0500

    (Update): testing glfw3

--- source/simKeyboard.cpp
+++ source/simKeyboard.cpp
@@ -2,10 +2,16 @@
 
 #include <simLib/simLib.h>
 
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
 #define PLUGIN_VERSION 6
 
 // NOLINTNEXTLINE
 static LIBRARY simLib;
+
+static GLFWwindow* g_window = nullptr;
 
 SIM_DLLEXPORT auto simInit(SSimInit* info) -> int {
     simLib = loadSimLibrary(info->coppeliaSimLibPath);
@@ -26,6 +32,18 @@ SIM_DLLEXPORT auto simInit(SSimInit* info) -> int {
 
     simAddLog(info->pluginName, sim_verbosity_infos,
               "Successfully initialized plugin");
+
+    if (glfwInit() != GLFW_TRUE) {
+        return 0;
+    }
+
+    g_window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (g_window == nullptr) {
+        glfwTerminate();
+        return 0;
+    }
+
+    std::cout << "Hello World!" << std::endl;
 
     return PLUGIN_VERSION;
 }
